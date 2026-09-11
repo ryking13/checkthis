5e3b5fc672d7d9a88ff0f58ea3e02df881d8251c
ryking13 <ryking13@gmail.com>
2026-09-11 08:38:31 -0500

    Increase search window limit to 800 minutes

--- bot.py
+++ bot.py
@@ -75,7 +75,7 @@ PENDING_FILE = Path(__file__).parent / "pending_alerts.json"
 SEARCH_RESULT_LIMIT = 100
 
 # Search for listings from the last N minutes (with 1-minute overlap)[cite: 1]
-SEARCH_WINDOW_MINUTES = 6
+SEARCH_WINDOW_MINUTES = 800
 SEARCH_WINDOW_OVERLAP_MINUTES = 1
 
 # --- Quiet hours ---
